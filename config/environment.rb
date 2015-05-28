d4f6a08fd1c1ced84977a2c9bf1721f25dedbe72
Roman Kozvonin <romankozvonin@gmail.com>
2015-05-28 21:38:44 +0400

    28-05-2015 21:38

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
+Time::DATE_FORMATS[:ru_datetime] = "%d.%m.%Y в %k:%M:%S"
 
 # Initialize the Rails application.
 Rails.application.initialize!
