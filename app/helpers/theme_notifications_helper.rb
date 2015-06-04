c806c26dc7c0046bfc0d839c80f6509488a39a97
Roman Kozvonin <romankozvonin@gmail.com>
2015-06-04 21:20:50 +0400

    21-20-2015 editing ThemeNoticationsHelper

--- app/helpers/theme_notifications_helper.rb
+++ app/helpers/theme_notifications_helper.rb
@@ -4,7 +4,7 @@ module ThemeNotificationsHelper
 		if signed_in?
 			nt = current_user.getThemeNotification(theme_id)
 			if nt == nil
-				v = "<a id = 'watchTheme' th_id = '#{theme_id}' class = 'b_link pointer'>Отслеживать</a>"
+				v = {:name => "Отслеживать тему", :access => signed_in?, :type => 'edit', :id => 'watchTheme'}# "<a id = 'watchTheme' th_id = '#{theme_id}' class = 'b_link pointer'>Отслеживать</a>"
 			else
 				v = "<a id = 'unWatchTheme' th_id = '#{theme_id}' class = 'b_link pointer'>Не отслеживать</a>"
 			end
